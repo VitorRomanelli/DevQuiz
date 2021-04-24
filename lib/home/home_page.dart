b64d0ca29b185f97bdf4605b7421b3201a6e8796
VitorRomanelli <vitorvromanelli@gmail.com>
2021-04-24 17:37:25 -0300

    Adicionando interaçoes e database

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -1,6 +1,12 @@
+import 'package:DevQuiz/challenge/widgets/quiz/quiz_widget.dart';
 import 'package:DevQuiz/core/app_colors.dart';
 import 'package:DevQuiz/home/widgets/app_bar/app_bar_widget.dart';
+import 'package:DevQuiz/home/widgets/level_button/level_button_widget.dart';
+import 'package:DevQuiz/home/widgets/quiz_card/quiz_card_widget.dart';
 import 'package:flutter/material.dart';
+
+import 'home_controller.dart';
+import 'home_state.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -8,10 +14,80 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final controller = HomeController();
+
+  @override
+  void initState() {
+    super.initState();
+
+    controller.getUser();
+    controller.getQuizzes();
+
+    controller.stateNotifier.addListener(() {
+      setState(() {});
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBarWidget(),
-    );
+    if (controller.state == HomeState.success) {
+      return Scaffold(
+        appBar: AppBarWidget(user: controller.user!),
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              SizedBox(
+                height: 24,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  LevelButtonWidget(
+                    label: "Fácil",
+                  ),
+                  LevelButtonWidget(
+                    label: "Médio",
+                  ),
+                  LevelButtonWidget(
+                    label: "Difícil",
+                  ),
+                  LevelButtonWidget(
+                    label: "Perito",
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 24,
+              ),
+              Expanded(
+                child: GridView.count(
+                  mainAxisSpacing: 8,
+                  crossAxisSpacing: 8,
+                  crossAxisCount: 2,
+                  children: controller.quizzes!
+                      .map(
+                        (e) => QuizCardWidget(
+                            title: e.title,
+                            completed:
+                                "${e.questionAnswered}/${e.questions.length}",
+                            percentage:
+                                e.questionAnswered / e.questions.length),
+                      )
+                      .toList(),
+                ),
+              ),
+            ],
+          ),
+        ),
+      );
+    } else {
+      return Scaffold(
+          body: Center(
+        child: CircularProgressIndicator(
+          valueColor: AlwaysStoppedAnimation<Color>(AppColors.darkGreen),
+        ),
+      ));
+    }
   }
 }
--- lib/home/widgets/app_bar/app_bar_widget.dart
+++ lib/home/widgets/app_bar/app_bar_widget.dart
@@ -1,9 +1,12 @@
 import 'package:DevQuiz/core/core.dart';
 import 'package:DevQuiz/home/widgets/score_card/score_card_widget.dart';
+import 'package:DevQuiz/shared/models/user_model.dart';
 import 'package:flutter/material.dart';
 
 class AppBarWidget extends PreferredSize {
-  AppBarWidget()
+  final UserModel user;
+
+  AppBarWidget({required this.user})
       : super(
           preferredSize: Size.fromHeight(250),
           child: Container(
@@ -24,8 +27,9 @@ class AppBarWidget extends PreferredSize {
                           style: AppTextStyles.title,
                           children: [
                             TextSpan(
-                                text: "Vitor Romanelli",
-                                style: AppTextStyles.titleBold),
+                              text: user.name,
+                              style: AppTextStyles.titleBold,
+                            ),
                           ],
                         ),
                       ),
@@ -36,12 +40,16 @@ class AppBarWidget extends PreferredSize {
                           borderRadius: BorderRadius.circular(10),
                           image: DecorationImage(
                             image: NetworkImage(
-                                "https://avatars.githubusercontent.com/u/68878604?v=4"),
+                              user.photoUrl,
+                            ),
                           ),
                         ),
                       )
                     ],
                   ),
+                ),
+                SizedBox(
+                  height: 24,
                 ),
                 Align(
                   alignment: Alignment(0.0, 1.0),
